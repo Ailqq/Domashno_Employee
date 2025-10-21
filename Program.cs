98d4dbaf7361ae87bf7c9d5241f0fe6db7dfa486
Ailqq <aylasali243@gmail.com>
2025-10-21 17:26:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeHW
+{
+    internal class Program
+    {
+        public class Employee
+        {
+           private string FullName;
+           private int Salary;
+           private int YearsOfExperience;
+
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
